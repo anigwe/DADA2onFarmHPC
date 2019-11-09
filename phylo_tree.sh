a29e7b975f1ac1c476382acc22ef51bb54ddfc1b
anigwe <7919212+anigwe@users.noreply.github.com>
2019-11-09 00:56:22 -0800

    Add files via upload

---
+++ phylo_tree.sh
@@ -0,0 +1,14 @@
+#!/bin/bash -l
+
+#SBATCH --job-name=ctri-tree
+#SBATCH -o ctri-tree.%j.%N.out
+#SBATCH -e ctri-tree.err
+#SBATCH -p bigmemht
+#SBATCH --ntasks=16
+#SBATCH --time=288:00:00
+#SBATCH --mail-type=ALL
+#SBATCH --mail-user=aigwe@ucdavis.edu
+
+#SBATCH --export==ALL
+
+Rscript /home/aigwe/CTRI/ctri_phylo_tree.R
